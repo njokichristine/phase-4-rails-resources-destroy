df1ea9d23516690b98c1dcc6486b9ea7b24fa184
njokichristine <christinenjoki6900@gmail.com>
2023-03-20 11:55:02 +0300

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,3 +1,4 @@
+
 class BirdsController < ApplicationController
 
   # GET /birds
@@ -41,6 +42,18 @@ class BirdsController < ApplicationController
       render json: bird
     else
       render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  # DELETE /birds/:id
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+      render json: {status: "deleted succesfully"}
+    else
+      render json: {error: "Bird not found"}, status: :not_found
     end
   end
 
